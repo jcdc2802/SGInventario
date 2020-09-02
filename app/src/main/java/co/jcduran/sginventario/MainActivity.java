5153c11699fdab1aa262fdce7a4424ed08683dd0
jcdc2802 <69880704+jcdc2802@users.noreply.github.com>
2020-09-02 02:25:14 +0000

    declaracionde un string

--- app/src/main/java/co/jcduran/sginventario/MainActivity.java
+++ app/src/main/java/co/jcduran/sginventario/MainActivity.java
@@ -7,15 +7,16 @@ import android.view.Window.*;
 
 public class MainActivity extends Activity
 {
+	String strClave;
 	
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    	@Override
+    	protected void onCreate(Bundle savedInstanceState)
+    	{
+        	super.onCreate(savedInstanceState);
+        	setContentView(R.layout.main);
 		
-	//setContentView(R.layout.inicio_fragment);
-    }
+		//setContentView(R.layout.inicio_fragment);
+	 }
 	
 	
 }
