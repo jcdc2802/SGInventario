a782ead7bc0c51c9ac1707b72ec4a776b027e3d7
Julio Cesar Duran Chavez <jc.dc2802@gmail.com>
2020-09-01 21:51:49 -0400

    se añade fragment

--- app/src/main/java/co/jcduran/sginventario/MainActivity.java
+++ app/src/main/java/co/jcduran/sginventario/MainActivity.java
@@ -2,13 +2,20 @@ package co.jcduran.sginventario;
 
 import android.app.*;
 import android.os.*;
+import co.jcduran.sginventario.fragment.*;
+import android.view.Window.*;
 
-public class MainActivity extends Activity 
+public class MainActivity extends Activity
 {
+	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		setContentView(R.layout.inicio_fragment);
     }
+	
+	
 }
---
+++ app/src/main/java/co/jcduran/sginventario/fragment/InicioFragment.java
@@ -0,0 +1,16 @@
+package co.jcduran.sginventario.fragment;
+import android.app.*;
+import android.os.*;
+import co.jcduran.sginventario.*;
+
+public class InicioFragment extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.inicio_fragment);
+	}
+	
+}
