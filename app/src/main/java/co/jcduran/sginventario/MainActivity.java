dba6e2ae2e3d035752eca7aa5db162ba38bd8670
jcdc2802 <69880704+jcdc2802@users.noreply.github.com>
2020-09-02 01:55:29 +0000

    se quita el fragment

--- app/src/main/java/co/jcduran/sginventario/MainActivity.java
+++ app/src/main/java/co/jcduran/sginventario/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 		
-		setContentView(R.layout.inicio_fragment);
+	//setContentView(R.layout.inicio_fragment);
     }
 	
 	
